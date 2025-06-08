5e5ed35d42d9e2a859f8884a98f6be3b6e7d5b6e
Ran Almagor <ranalmagor100@gmail.com>
2025-06-08 21:33:10 +0300

    ק

--- DefusuonCraft_ClientSide/js/navbar-loader.js
+++ DefusuonCraft_ClientSide/js/navbar-loader.js
@@ -1,9 +1,17 @@
-// File: js/navbar-loader.js
-
+// js/navbar-loader.js
 document.addEventListener("DOMContentLoaded", () => {
   fetch("web components/navbar.html")
     .then((res) => res.text())
     .then((html) => {
       document.getElementById("navbar-container").innerHTML = html;
+
+      // רק אחרי שה-navbar נטען לדף נטען גם את הסקריפט שמפעיל את ההסתרה / הצגה
+      const script = document.createElement("script");
+      script.src = "js/auth-handler.js";
+      script.defer = true;
+      document.body.appendChild(script);
+    })
+    .catch((err) => {
+      console.error("Failed to load navbar:", err);
     });
 });
