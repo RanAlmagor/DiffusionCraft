38d7dbfd0e1585a0749daf8e45e1c95794435c06
Ran Almagor <ranalmagor100@gmail.com>
2025-06-17 18:06:24 +0300

    Ran

--- DefusuonCraft_ClientSide/js/delete-image.js
+++ DefusuonCraft_ClientSide/js/delete-image.js
@@ -1,25 +1,26 @@
 // === Delete Image from DynamoDB and S3 via API ===
 
+/**
+ * Sends a DELETE request to the API to delete an image using query parameters.
+ * @param {string} imageId - The ID of the image to delete.
+ */
 async function deleteImage(imageId) {
   const userInfo = JSON.parse(localStorage.getItem("userInfo") || "{}");
   const userSub = userInfo.name;
   const isAdmin = userInfo.groups?.includes("Admins") || false;
 
+  // Build URL with query parameters
+  const url = new URL(
+    "https://qw1foyfl98.execute-api.us-east-1.amazonaws.com/Prod/Images/Personal"
+  );
+  url.searchParams.append("imageId", imageId);
+  url.searchParams.append("userSub", userSub);
+  url.searchParams.append("isAdmin", isAdmin);
+
   try {
-    const response = await fetch(
-      "https://qw1foyfl98.execute-api.us-east-1.amazonaws.com/Prod/DeleteImage",
-      {
-        method: "POST", // Lambda is using POST even for delete behavior
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          imageId: imageId,
-          userSub: userSub,
-          isAdmin: isAdmin,
-        }),
-      }
-    );
+    const response = await fetch(url.toString(), {
+      method: "DELETE",
+    });
 
     const data = await response.json();
 
